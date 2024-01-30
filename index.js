12e06a794311b9bff9781522bcdfa881b949e427
Vanced Solutions <SanjayKumar@vancedsolutions.com>
2024-01-30 18:37:10 +0530

    Move index file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors')
-const ShortUrl = require("../model/shortUrl")
+const ShortUrl = require("./model/shortUrl")
 require('dotenv').config()
 const app = express()
 app.use(express.json())
