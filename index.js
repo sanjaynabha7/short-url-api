50d226eb869c0de12855c2fe94f61f9a8f1caf60
Sanjay Kumar <sanjaynabha7@gmail.com>
2024-02-12 12:41:40 +0530

    updates index,js

--- index.js
+++ index.js
@@ -1,12 +1,72 @@
 const express = require('express');
 const cors = require('cors')
+const ShortUrl = require("./model/shortUrl")
+require('dotenv').config()
 const app = express()
-app.use(cors())
 app.use(express.json())
-
-app.get("/api/testing", async (req, res) => { res.send("Working 0.1") });
-
-
-app.listen(9090, () => {
-  console.log("Server is running..." + 9090)
+app.use(cors())   
+const mongoose = require('mongoose')
+ 
+mongoose.connect(process.env.DATABASE, { useNewUrlParser: true, useUnifiedTopology: true, }).then(() => {
+    console.log("Connected")
+}).catch((e) => {
+    console.log(e, "Error Mongoose ")
 })
+
+
+app.post("/convert-short-url", (req, res) => {
+    let data = req.body
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for (var i = 0; i < 5; i++)
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    const payload = {
+        fullUrl: data.fullUrl,
+        shortUrl: text
+    }
+    ShortUrl.create(payload).then(createdRecord => {
+        return res.status(200).send({
+            status: 200,
+            msg: "ok",
+            data: createdRecord,
+        });
+    }).catch(e => {
+        res.status(400).send({
+            status: 400,
+            msg: "error",
+            data: e,
+        });
+    });
+});
+
+
+
+app.get("/get-url", (req, res) => {
+    ShortUrl.find().then(createdRecord => {
+        return res.status(200).send({
+            status: 200,
+            msg: "ok",
+            data: createdRecord,
+        });
+    }).catch(e => {
+        res.status(400).send({
+            status: 400,
+            msg: "error",
+            data: e,
+        });
+    });
+});
+
+app.get("/testing", (req, res) => {
+        res.status(200).send({
+            status: 200,
+            msg: "Working",
+        });
+
+});
+
+
+const PORT = 5000
+app.listen(PORT, () => {
+    console.log(`Server is running at ${PORT}`)
+}) 
