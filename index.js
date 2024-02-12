bb55ee65041d5fe3b8f31cab108a80d58ec51740
Sanjay Kumar <sanjaynabha7@gmail.com>
2024-02-12 12:32:54 +0530

    update index 85

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 const express = require('express');
 const cors = require('cors')
 const app = express()
-require('dotenv').config()
-const PORT =  process.env.PORT;
 app.use(cors())
 app.use(express.json())
 
 app.get("/api/testing", async (req, res) => { res.send("Working 0.1") });
 
-// Serve static files from the 'public' directory
-// app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use(express.static("../public"));
-app.use("/public", express.static("./public"));
-
-app.listen(PORT, () => {
-  console.log("Server is running..." + PORT)
+app.listen(9090, () => {
+  console.log("Server is running..." + 9090)
 })
